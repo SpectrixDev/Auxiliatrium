d053fa15d942fe7df8c8222225128d77f2904b1b
SpectrixOfficial <owenhutch123@gmail.com>
2018-08-20 20:06:25 +0200

    Starting to look hot

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+function searchYouTube(){
+    if (event.keyCode == 13 || event.which == 13) { location='http://www.youtube.com/search?q=' + encodeURIComponent(document.getElementById('textbox').value);}
+}
+
+function searchGoogle(){
+    if (event.keyCode == 13 || event.which == 13) { location='http://www.youtube.com/search?q=' + encodeURIComponent(document.getElementById('textbox').value);}
+}
+
+function searchGitHub(){
+    if (event.keyCode == 13 || event.which == 13) { location='http://www.youtube.com/search?q=' + encodeURIComponent(document.getElementById('textbox').value);}
+}

03c7059a1213b53cd77777ce0f80ec3d8c80de5b
SpectrixOfficial <owenhutch123@gmail.com>
2018-08-22 20:20:49 +0200

    Added time and greeting

--- scripts/main.js
+++ scripts/main.js
@@ -14,3 +14,33 @@ function gitH(){
     getLocation = 'https://www.github.com/search?q=';
     location = getLocation + encodeURIComponent(document.getElementById('textbox').value);
 }
+
+
+function startTime() {
+    var today = new Date();
+    var greeting;
+    var h = today.getHours();
+    if(h < 12) {
+        greeting = "Good morning.";
+    }
+    else if(h >= 12 && h < 17) {
+        greeting = "Good afternoon.";
+    }
+    else{
+        greeting = "Good evening.";
+    }
+
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('clock').innerHTML =
+    h + ":" + m + ":" + s;
+    document.getElementById('greetingMsg').innerHTML = greeting;
+    var t = setTimeout(startTime, 500);
+
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}

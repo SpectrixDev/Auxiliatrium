d34edec4dbf28cc9fa5a1419c5223bd084dae582
woomybyte <samgaming2005@gmail.com>
2018-08-27 14:08:05 -0400

    lol
    ok this shits scss now and idk ok my code blows. also renamed the logos to make more sense and added a gitignore.

--- scripts/main.js
+++ scripts/main.js
@@ -1,87 +1,40 @@
-var getLocation = "https://www.google.com/search?q="
+let currentLocation;
 
-function search(getLocation){
-    location = getLocation + encodeURIComponent(document.getElementById('textbox').value);
-}
+const locations = [
+  {
+    name:"Google",
+    location:"https://google.com/((search?q=QQQ))"
+  },
+  {
+    name:"Twitter",
+    location:"https://twitter.com/((search?q=QQQ))"
+  },
+  {
+    name:"Reddit",
+    location:"https://www.reddit.com/((search?q=QQQ))"
+  }
+];
 
-function reddit(){
-    var txtBoxInput = document.getElementById('textbox').value
-    if (txtBoxInput == '' || txtBoxInput == ' ') {
-        location = "https://www.reddit.com/"
-    }
-    else if (txtBoxInput.substring(0, 2) == "r/") {
-        location = "https://www.reddit.com/" + document.getElementById('textbox').value;
-    }
-    else {
-        location = "https://www.reddit.com/search?q=" + encodeURIComponent(document.getElementById('textbox').value);
-    }
-}
+const search = () => {
+  const input = document.getElementById('textbox').value;
+  const matches = locations[currentLocation].location.match(/(.+)\({2}(.+)\){2}/);
+  if (/\S/.test(input)) {
+    location = matches[1] + matches[2].replace("QQQ", encodeURIComponent(input));
+  }
+  else {
+    location = matches[1];
+  }
+};
 
-function stackoverflow(){
-    var txtBoxInput = document.getElementById('textbox').value
-    if (txtBoxInput == '' || txtBoxInput == ' ') {
-        location = "https://stackoverflow.com/";
-    }
-    else {
-        location = "https://stackoverflow.com/search?q=" + encodeURIComponent(document.getElementById('textbox').value);
-    }
-}
-
-function gmail(){
-    var txtBoxInput = document.getElementById('textbox').value
-    if (txtBoxInput == '' || txtBoxInput == ' ') {
-        location = "https://mail.google.com/mail/u/0/#inbox";
-    }
-    else {
-        location = "https://mail.google.com/mail/u/0/#search/" + encodeURIComponent(document.getElementById('textbox').value);
-    }
-}
-
-function twitter(){
-    var txtBoxInput = document.getElementById('textbox').value
-    if (txtBoxInput == '' || txtBoxInput == ' ') {
-        location = "https://twitter.com/";
-    }
-    else {
-        location = "https://twitter.com/search?q=" + encodeURIComponent(document.getElementById('textbox').value);
-    }
-}
-
-function github(){
-    var txtBoxInput = document.getElementById('textbox').value
-    if (txtBoxInput == '' || txtBoxInput == ' ') {
-        location = "https://github.com/";
-    }
-    else {
-        location = "https://github.com/search?q=" + encodeURIComponent(document.getElementById('textbox').value);
-    }
-}
-
-function startTime() {
-    var today = new Date();
-    var greeting;
-    var h = today.getHours();
-    if(h < 12) {
-        greeting = "Good morning.";
-    }
-    else if(h >= 12 && h < 17) {
-        greeting = "Good afternoon.";
-    }
-    else {
-        greeting = "Good evening.";
-    }
-
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('clock').innerHTML =
-    h + ":" + m + ":" + s;
-    document.getElementById('greetingMsg').innerHTML = greeting;
-    var t = setTimeout(startTime, 500);
-
-}
-function checkTime(i) {
-    if (i < 10) {i = "0" + i};
-    return i;
-}
+const start = () =>{
+  currentLocation = 0;
+      
+  locations.forEach(loc=>{
+    const node = document.createElement("button");
+    const textnode = document.createTextNode(loc.name);
+    node.classList.add("buttonstyle");
+    node.onclick = new Function(`currentLocation = ${locations.indexOf(loc)}; search()`);
+    node.appendChild(textnode);
+    document.getElementById("buttons").appendChild(node);
+  });
+};

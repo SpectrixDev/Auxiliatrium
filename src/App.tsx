1c0951634d5baf5e225f303ff7a6782d1f1b9f92
Spectrix <owenhutch123@gmail.com>
2022-04-30 17:40:27 +0100

    Random stuff idk

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { Component } from "react";
+import { Table, Button } from "reactstrap";
+import Edit from "./Edit";
 import logo from './logo.png';
 import './App.css';
 
-function App() {
-  document.body.style.overflow = "hidden"
-  return (
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      names: ["YouTube", "GitHub", "Gcal", "Gmail", " Twitter"],
+      showbutton: false,
+      showButtonIndex: null
+    };
+    this.handleEdit = this.handleEdit.bind(this);
+  }
+
+  handleEdit = e => {
+    e.preventDefault();
+    this.setState({
+      showbutton: true,
+      showButtonIndex: Number(e.target.id)
+    });
+    console.log(this.state)
+  };
+  render() {
+    console.log(this.state.names);
+    const { showButtonIndex, showbutton } = this.state;
+    return (
     <div className="App">
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
       <div className="animate__animated animate__fadeIn">
@@ -27,12 +49,13 @@ function App() {
             <div className="secondRowButtons">
               <button className="buttons">Reddit</button>
               <button className="buttons">Last.fm</button>
-              <button className="buttons">test</button>
+              <button className="buttons">Twitch</button>
               <button className="buttons">test</button>
               <button className="buttons">test</button>
             </div>
+            <button className="buttons" id="home">Home</button>
     </div>
   );
-}
+}};
 
 export default App;
---
+++ src/Edit.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Edit = props => {
+  return (
+    window.open(newPageUrl, "_blank")
+  );
+};
+
+export default Edit;
